c4bfe775c5db4aae72e24525ca3d164bac240fed
Hezron-tech <kiprophez@gmail.com>
2022-04-08 23:51:17 +0300

    creating userfrom

--- awards/forms.py
+++ awards/forms.py
@@ -11,3 +11,11 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email' )       
