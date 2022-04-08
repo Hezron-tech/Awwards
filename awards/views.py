cdf0194e01db8ecdb95abbb716ff4e74e038d641
Hezron-tech <kiprophez@gmail.com>
2022-04-08 22:49:19 +0300

    adding profile method

--- awards/views.py
+++ awards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from .models import Project,Profile,Rates
 
@@ -8,3 +9,11 @@ def index(request):
     profile= Profile.objects.all()
     projects= Project.objects.all()
     return render(request,'index.html',{"profile":profile, "projects":projects})
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    projects = Project.objects.filter(user=current_user.id).all
+    return render(request, 'registration/profile.html', {"projects": projects})
+
