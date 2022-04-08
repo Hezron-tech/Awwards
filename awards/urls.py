30a54040b78b1b63fd7da7c1af0ce253c1205eae
Hezron-tech <kiprophez@gmail.com>
2022-04-08 22:35:30 +0300

    index view

---
+++ awards/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.conf.urls import url
+
+
+
+urlpatterns=[
+    url('^$',views.index,name = 'index'),
+
+
+]
--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Project,Profile,Rates
+
 # Create your views here.
+
+def index(request):
+    profile= Profile.objects.all()
+    projects= Project.objects.all()
+    return render(request,'index.html',{"profile":profile, "projects":projects})
