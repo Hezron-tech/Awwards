537e67deaa64eb84758ab695a874861c1ae43595
Hezron-tech <kiprophez@gmail.com>
2022-04-08 23:06:34 +0300

    creating post project function

--- awards/views.py
+++ awards/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from .forms import ProjectForm,RatingsForm,SignUpForm, UpdateProfileForm, UpdateUserForm
 
 from .models import Project,Profile,Rates
 
@@ -23,3 +24,22 @@ def project(request, id):
     project = Project.objects.get(id=id)
     reviews = Rates.objects.all()
     return render(request, 'view-project.html', {"project": project, "reviews": reviews})
+
+@login_required(login_url='/accounts/login')
+def post_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = projectForm(request.POST,request.FILES)
+        if form.is_valid():
+            post_project = form.save(commit=False)
+            post_project.user = current_user
+            post_project.save()
+            return redirect('index')
+
+        else:
+            form = projectForm()
+        return render(request,'projects.html',{"form":form})        
+
+
+
+
